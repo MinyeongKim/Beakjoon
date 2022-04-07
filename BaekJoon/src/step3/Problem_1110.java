42fdfaccd6cd5830fbd79793a69494ece90675e0
MinyeongKim <32475904+MinyeongKim@users.noreply.github.com>
2022-04-07 16:45:40 +0900

    step3

---
+++ BaekJoon/src/step3/Problem_1110.java
@@ -0,0 +1,36 @@
+package step3;
+import java.util.Scanner;
+
+public class Problem_1110 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		int N_temp = N;
+		int cnt = 0;
+		
+		if(N<0 || N>99) {
+			return;
+		}
+		
+		while(true) {
+				
+			if(N<10) {
+				N = (N%10)*10 + N;
+			} else {
+				int A = N/10;
+				int B = N%10;
+				int temp = A+B;
+				
+				N = (B*10) + (temp%10);
+			}
+			cnt++;
+			
+			if(N==N_temp) {
+				System.out.println(cnt);
+				break;
+			}
+			
+		}
+		
+	}
+}
---
+++ BaekJoon/src/step3/Problem_2438.java
@@ -0,0 +1,16 @@
+package step3;
+import java.util.Scanner;
+
+public class Problem_2438 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
